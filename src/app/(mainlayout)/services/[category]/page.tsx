0e675b715b546a5cd0caa843ebe7149169e481e6
liton61 <litonofficial61@gmail.com>
2024-11-19 14:30:02 +0600

    services page done

--- src/app/(mainlayout)/services/[category]/page.tsx
+++ src/app/(mainlayout)/services/[category]/page.tsx
@@ -1,89 +1,94 @@
-// "use client";
+"use client";
 
-// import { useEffect, useState } from "react";
-// import { useParams } from "next/navigation";
-// import Image from "next/image";
-// import Container from "@/components/ui/Container/Container";
-// import Banner from "@/components/shared/Banner/Banner";
+import Banner from "@/components/shared/Banner/Banner";
+import Container from "@/components/ui/Container/Container";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Sidebar from "../_components/Sidebar";
 
+type Product = {
+  _id: string;
+  title: string;
+  sub_title: string;
+  description: string;
+  category: string[];
+  images: string[];
+};
 
-// type Product = {
-//   _id: string;
-//   title: string;
-//   category: string[];
-//   images: string[];
-// };
+type ApiResponse = {
+  data: {
+    services: Product[];
+  };
+};
 
-// type ApiResponse = {
-//   data: {
-//     services: Product[];
-//   };
-// };
+const CategoryPage = () => {
+  const { category } = useParams<{ category: string }>();
+  const [data, setData] = useState<Product[]>([]);
+  const [filteredService, setFilteredService] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
 
-// const CategoryPage = () => {
-//   const { category } = useParams<{ category: string }>();
-//   const [data, setData] = useState<Product[]>([]);
-//   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-//   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/v1/services");
+        const result = (await response.json()) as ApiResponse;
+        setData(result?.data?.services || []);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
 
-//   useEffect(() => {
-//     fetch("http://localhost:5000/api/v1/services")
-//       .then((response) => response.json() as Promise<ApiResponse>)
-//       .then((data) => {
-//         setData(data?.data?.services || []);
-//         setLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//         setLoading(false);
-//       });
-//   }, []);
+    fetchServices();
+  }, []);
 
-//   console.log(data);
+  useEffect(() => {
+    if (category && data.length > 0) {
+      const decodedCategory = decodeURIComponent(category);
 
-//   useEffect(() => {
-//     if (category && data.length > 0) {
-//       const decodedCategory = decodeURIComponent(category);
+      // Find the service matching the category
+      const service = data.find((product) => product.title === decodedCategory);
 
-      
-//       const filtered = data.filter((product) =>
-//         product.category.some(
-//           (cat) => cat.toLowerCase() === decodedCategory.toLowerCase()
-//         )
-//       );
-//       setFilteredProducts(filtered);
-//     }
-//   }, [category, data]);
+      setFilteredService(service || null);
+    }
+  }, [category, data]);
 
-//   if (loading) {
-//     return <div className="text-center py-20">Loading...</div>;
-//   }
+  if (loading) {
+    return <div className="text-center py-20">Loading...</div>;
+  }
 
-//   return (
-//     <div className="bg-gray-100">
-//       <Banner title={`Category: ${category}`} />
-//       <Container className="py-20">
-//         <h1 className="text-3xl font-bold my-10 text-center capitalize">
-//           {category}
-//         </h1>
-//         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-//           {filteredProducts.map((product) =>
-//             product?.images?.slice(0,1).map((image, index) => (
-//               <div key={index} className="relative">
-//                 <Image
-//                   src={image}
-//                   alt={product.title}
-//                   width={300}
-//                   height={300}
-//                   className="w-full h-auto object-cover rounded-lg shadow-md"
-//                 />
-//               </div>
-//             ))
-//           )}
-//         </div>
-//       </Container>
-//     </div>
-//   );
-// };
+  if (!filteredService) {
+    return (
+      <div className="text-center py-20">
+        <p className="text-red-500">No data found !</p>
+      </div>
+    );
+  }
 
-// export default CategoryPage;
+  return (
+    <>
+      <Banner title={category} />
+      <Container className="my-20">
+        <div className="lg:flex gap-10">
+          <Sidebar />
+          <div className="w-full">
+            <h2 className="text-2xl font-bold mb-3">{filteredService.title}</h2>
+            <p className="mb-3">{filteredService.sub_title}</p>
+            <Image
+              src={filteredService.images[0]}
+              alt={filteredService.title}
+              className="w-full max-h-[500px] object-cover my-10"
+              width={600}
+              height={400}
+            />
+            <p className="text-justify">{filteredService.description}</p>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default CategoryPage;
--- src/app/(mainlayout)/services/_components/Sidebar.tsx
+++ src/app/(mainlayout)/services/_components/Sidebar.tsx
@@ -1,137 +1,50 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 
 const Sidebar = () => {
   const currentRoute = usePathname();
 
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/services")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data?.data?.services || []);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(data);
+
+  if (loading) {
+    return <div className="text-center py-20">Loading...</div>;
+  }
+
   return (
     <div className="w-full lg:w-96 space-y-3">
-      <Link
-        href="/services/research"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-4 ${
-          currentRoute === "/services/research" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>Research & Development</span>
-        <IoIosArrowForward />
-      </Link>
-
-      <Link
-        href="/services/development"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/development"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Sample Development</span>
-        <IoIosArrowForward />
-      </Link>
-
-      <Link
-        href="/services/merchandising"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/merchandising"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Merchandising</span>
-        <IoIosArrowForward />
-      </Link>
-
-      <Link
-        href="/services/fab-sourcing"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/fab-sourcing"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Fabric Sourcing</span>
-        <IoIosArrowForward />
-      </Link>
-
-      <Link
-        href="/services/production"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/production"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Production</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/qa-qc"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/qa-qc" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>QA and QC</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/delivery"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/delivery" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>Delivery and Shipment</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/knitting"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/knitting" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>Knitting</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/embroidery"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/embroidery"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Embroidery</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/dyeing-wash"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/dyeing-wash"
-            ? "bg-[#02ADFF] text-white"
-            : ""
-        }`}
-      >
-        <span>Garment Dyeing & Wash</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/printing"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/printing" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>Printing</span>
-        <IoIosArrowForward />
-      </Link>
-      <Link
-        href="/services/trimming"
-        className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
-          currentRoute === "/services/trimming" ? "bg-[#02ADFF] text-white" : ""
-        }`}
-      >
-        <span>Trimming & Accessories</span>
-        <IoIosArrowForward />
-      </Link>
+      {data.map((item: { _id: string; title: string }) => (
+        <Link
+          key={item?._id}
+          href={`/services/${item?.title}`}
+          className={`bg-gray-100 flex items-center justify-between px-4 py-3 ${
+            currentRoute === "/services/trimming"
+              ? "bg-[#02ADFF] text-white"
+              : ""
+          }`}
+        >
+          {item?.title}
+          <IoIosArrowForward />
+        </Link>
+      ))}
     </div>
   );
 };
